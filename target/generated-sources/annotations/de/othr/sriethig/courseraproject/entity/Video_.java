0dbc84d2bda1a1d0ae42b6712673e9f012e111cf
Sonja Riethig <sriethig@googlemail.com>
2017-01-24 21:42:20 +0100

    commit before trying to add sebis nutwork as webservice

--- target/generated-sources/annotations/de/othr/sriethig/courseraproject/entity/Video_.java
+++ target/generated-sources/annotations/de/othr/sriethig/courseraproject/entity/Video_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-01-19T22:06:25")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-01-24T21:37:09")
 @StaticMetamodel(Video.class)
 public class Video_ extends SingleIdEntity_ {
 
